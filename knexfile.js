028b32c85d3b2781b4b321ea59e36f9e8f3ce607
Jeff Kerpan <jkerpan@gmail.com>
2017-08-24 15:22:04 -0600

    created migrations, users, beers, locations

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,12 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection:
-      filename: 'postgres://localhost/99bottles'
+    connection:'postgres://localhost/99bottles'
   },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
   }
-  
+
 };
---
+++ migrations/20170824145642_users.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('user_name', 255).notNull().defaultTo('');
+    table.string('first_name', 255).notNull().defaultTo('');
+    table.string('last_name', 255).notNull().defaultTo('');
+    table.specificType('hashed_password', 'char(60)').notNull();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
